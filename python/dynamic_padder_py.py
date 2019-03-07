b8a9ea399733a653fc9d22f3392df58205661ae6
Gokhan <secinti@gmail.com>
2019-03-06 21:49:07 -0500

    dynamic_padder coded and tested.

--- python/dynamic_padder_py.py
+++ python/dynamic_padder_py.py
@@ -21,24 +21,74 @@
 
 import numpy
 from gnuradio import gr
+import numpy
+import pmt
 
 class dynamic_padder_py(gr.basic_block):
     """
     docstring for block dynamic_padder_py
     """
-    def __init__(self, iq_sample):
+    def __init__(self, real, imag):
         gr.basic_block.__init__(self,
             name="dynamic_padder_py",
             in_sig=[numpy.complex64],
             out_sig=[numpy.complex64])
 
+
+        complex_value = complex(float(real) ,float(imag))
+        self.padding_sample = numpy.complex64(complex_value)
+        print "Recevied sample: {}".format(self.padding_sample)
+
+        self.padding = False
+
+        # Init input message port
+        self.message_port_register_in(pmt.intern("trigger"))
+        self.set_msg_handler(pmt.intern("trigger"), self.set_padding)
+
     def forecast(self, noutput_items, ninput_items_required):
+        print "forecast called noutput_items:{}".format(noutput_items)
         #setup size of input_items[i] for work call
         for i in range(len(ninput_items_required)):
             ninput_items_required[i] = noutput_items
 
     def general_work(self, input_items, output_items):
-        output_items[0][:] = input_items[0]
-        consume(0, len(input_items[0]))
-        #self.consume_each(len(input_items[0]))
+        print "General work called in:{} - out:{}".format(len(input_items[0]), len(output_items[0]))
+
+        if self.padding is True:
+            sub_input = input_items[0][:len(output_items[0]) -self.length ]
+
+            output = numpy.append(sub_input, self.get_padding_array(self.length))
+            #numpy.append (
+            #    input_items[0][:len(output_items[0]) -self.length ],
+            #    self.get_padding_array(self.length))
+
+#            print output
+#            print len(output)
+            output_items[0][:] = output
+            self.consume(0, len(output_items[0])-self.length)
+
+            self.padding = False
+        else:
+            output_items[0][:] = input_items[0][:len(output_items[0])]
+            self.consume(0, len(output_items[0]))
+            #self.consume_each(len(input_items[0]))
+
         return len(output_items[0])
+
+    def set_padding(self, msg):
+
+        self.length = int(pmt.symbol_to_string(msg))
+
+        print(self.length)
+        self.padding = True
+
+
+    def get_padding_array(self, length):
+        index = 0
+        payload = []
+
+        while index < length:
+            payload.append(self.padding_sample)
+            index = index +1
+
+        return payload
